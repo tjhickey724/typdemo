57c387ffbd8e764b63a22bc80e8768f3b3500bfa
William Tarimo <wtarimo@brandeis.edu>
2015-12-01 11:58:53 -0500

    Added the ability to add orders with Saint Cyr

--- client/printer.js
+++ client/printer.js
@@ -2,22 +2,35 @@ Template.printerPage.events({
   "submit #orderform": function(event){
     
     event.preventDefault();
-    
-    var body = $("#comment-body").val();
-    
-    $("#comment-body").val("");
+    var page_count = $("#page_count").val();
+    if (this.duplex=="Yes") {
+      var duplex = $("#duplex").val();
+      $("#duplex").val("");
+    }
+    else {
+      var duplex = "No";
+    }
+    if (this.color=="Yes") {
+      var color = $("#color").val();
+      $("#color").val("");
+    }
+    else {
+      var color = "No";
+    }
+    $("#page_count").val("");
 
     var profile = Meteor.user().profile;
     
-    var comment = 
-        {
-        uid:Meteor.userId(),  
+    var order = {
+        customerId:Meteor.userId(),  
         who:profile["firstName"]+" "+profile["lastName"], 
-        body:body,
-        pid:this._id,
+        page_count:page_count,
+        printerId:this._id,
+        color:color,
+        duplex:duplex,
         when: new Date()
       };    
-    Comments.insert(comment);
+    Orders.insert(order);
   }
 });
 
@@ -25,8 +38,14 @@ Template.printerPage.helpers({
   notOwner: function(){
     return Meteor.userId() != this.ownerId;
   },
+  duplexPrinter: function(){
+    return this.duplex=="Yes";
+  },
+  colorPrinter: function(){
+    return this.color=="Yes";
+  },
   submittedOrders: function(){
-    return Orders.find({customerId:Meteor.userId()});
+    return Orders.find({customerId:Meteor.userId(),printerId:this._id});
   },
   receivedOrders: function(){
     return Orders.find({printerId:this._id});
